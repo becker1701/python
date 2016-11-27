657d3c989aba6992f3b8f7fe28687ef7b8515208
Brian Becker <becker1701@gmail.com>
2016-11-27 09:55:39 -0800

    commented run.py

---
+++ markov_chain/run.py
@@ -0,0 +1,22 @@
+"""
+
+The markov chain program will read quotes from http://www.goodreads.com/quotes/tag/being.
+
+The user will type in a word, and the program will search through all of the quotes pulled off www.goodreads.com and return a random string that immediatly proceeds that user entered word.
+
+The program functions like this:
+
+1. Setup program:
+    a: GET HTML page of quotes
+    b: parse all quotes into a local file
+2. Program prompts user for word to search for
+3. Program looks for the word in the quotes saved into the local file of quotes
+    a: Program creates a list of possible text; that is text that succeeds the user entered word
+    b: program will randomly select one of the found phrases
+    c: program prints the user enetered word and the random phrase
+    d: if the word isn't found, three randomly selected words are picked from the file and used a suggestions for the user
+    e: program will display the number of times the word was found in the file text
+4. Program repeats until user quits
+
+"""
+
